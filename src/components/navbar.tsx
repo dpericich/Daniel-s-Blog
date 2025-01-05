ba0f510f388267b8dc024c2b365d039c3af2dccf
Daniel Pericich <dpericich@gmail.com>
2025-01-05 15:52:19 -0600

    refactored navbar to move links to nav-drawer

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,33 +3,18 @@ import {
     Toolbar,
     Typography,
     Box,
-    IconButton,
 } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from "react-router-dom";
+
+import NavDrawer from './nav-drawer';
 
 function Navbar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    {/* If this goes mobile, I use the IconButton w/ menu and display my name in the navbar */}
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'space-between' }}>
-                        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Home</Link>
-                        <Link to="/projects" style={{ textDecoration: 'none', color: 'inherit' }}>Software Projects</Link>
-                        <Link to="/technical-writing" style={{ textDecoration: 'none', color: 'inherit' }}>Technical Blog</Link>
-                        <Link to="/teaching" style={{ textDecoration: 'none', color: 'inherit' }}>Teaching & Tutoring</Link>
-                        <Link to="/about" style={{ textDecoration: 'none', color: 'inherit' }}>About Me</Link>
-                        <Link to="/contact" style={{ textDecoration: 'none', color: 'inherit' }}>Contact</Link>
+                    <NavDrawer />
+                    <Typography align="right">
+                        Daniel Pericich - Blog
                     </Typography>
                 </Toolbar>
             </AppBar>
