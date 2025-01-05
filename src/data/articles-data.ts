e4d31740c17fa9db2722714ccab48db1a08049b4
Daniel Pericich <dpericich@gmail.com>
2025-01-05 00:05:54 -0600

    created articles page data for cards

---
+++ src/data/articles-data.ts
@@ -0,0 +1,80 @@
+type ArticleEntry = {
+    title: string,
+    date: string,
+    blurb: string,
+    author: string,
+    link: string
+}
+
+export const ArticleData: ArticleEntry[] = [
+    {
+        title: 'How To Validate File Types for File Uploads in JavaScript — Front End',
+        date: 'March 24, 2024',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/how-to-validate-file-types-for-file-uploads-in-javascript-front-end-ce81b8675f68'
+    },
+    {
+        title: 'How to Skip React useEffect on Initial Render',
+        date: 'September 16, 2023',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/how-to-bypass-useeffect-on-your-first-page-render-c31b7ba112a7'
+    },
+    {
+        title: 'How to Get Around Netflix’s New Household Policy',
+        date: 'July 16, 2023',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/how-to-get-around-netflixs-new-household-policy-d2c41fa8ffd5'
+    },
+    {
+        title: 'How to use JavaScript’s FormData Object to Send Complex Data',
+        date: 'October 30, 2022',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/how-to-use-javascripts-formdata-object-to-send-complex-data-c07c31c95f6c'
+    },
+    {
+        title: 'How to Build Multi Type Multidimensional Arrays in TypeScript',
+        date: 'June 14, 2022',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/how-to-build-multi-type-multidimensional-arrays-in-typescript-a9550c9a688e'
+    },
+    {
+        title: 'Why does console.log() return ‘undefined’?',
+        date: 'February 28, 2022',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://blog.bitsrc.io/why-does-console-log-return-undefined-e06d44b4d0f8'
+    },
+    {
+        title: 'Performing Union and Intersection Operations in Ruby on Rails',
+        date: 'November 18, 2021',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/performing-union-and-intersection-operations-in-ruby-on-rails-3f56e03f2637'
+    },
+    {
+        title: 'Using the React Children Prop to Create Reusable Layout Components',
+        date: 'June 7, 2021',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://medium.com/nerd-for-tech/using-the-react-children-prop-to-create-reusable-layout-components-93667aedc881'
+    },
+    {
+        title: 'How to Convert Strings to Booleans in Ruby',
+        date: 'May 27, 2021',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://medium.com/geekculture/how-to-convert-strings-to-booleans-in-ruby-f17abb2f0a7b'
+    },
+    {
+        title: 'Using RSpec’s change Matcher to Test Record Creation',
+        date: 'May 20, 2021',
+        blurb: 'This is some data about my article',
+        author: 'Daniel Pericich',
+        link: 'https://dpericich.medium.com/using-rspecs-change-matcher-to-test-record-creation-9da2216224af'
+    },
+]
