e974b968630f7a8cca72afa1c44a3689ff3a2063
Daniel Pericich <dpericich@gmail.com>
2025-01-05 15:18:36 -0600

    added blurb and updated WritingPage component

--- src/pages/Writing.tsx
+++ src/pages/Writing.tsx
@@ -1,3 +1,5 @@
+import { ArticleData } from "../data/articles-data";
+
 const WritingPage = () => {
     return (
         <div className="">
@@ -6,6 +8,23 @@ const WritingPage = () => {
                 <li>A section describing my writing with a link to my Medium</li>
                 <li>A section with preview cards that link out to some of my articles</li>
             </ol>
+            {/* Need to refactor this to be its own cards */}
+            {ArticleData.map((article) => (
+                <div className="">
+                    <hr />
+                    <h3 className="">{article.title}</h3>
+                    <div className="">
+                        <b>Author:</b> {article.author} // add person icon
+                    </div>
+                    <div className="">
+                        <b>Date Published:</b> {article.date} // add date icon
+                    </div>
+                    <p>
+                        {article.blurb}
+                    </p>
+                    <a href={article.link} target="_blank">Read More...</a>
+                </div>
+            ))}
         </div>
     )
 };
